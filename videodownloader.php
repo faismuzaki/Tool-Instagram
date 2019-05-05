d1425ced5b8b803bb9f7ebeef5ff61cf3c68a45a
faismuzaki <34159640+faismuzaki@users.noreply.github.com>
2019-05-05 19:22:00 +0700

    Update videodownloader.php

--- videodownloader.php
+++ videodownloader.php
@@ -2,13 +2,12 @@
 
 	echo "Url Video : ";
 	$urlna = trim(fgets(STDIN));
-    					$ch = curl_init();
-						curl_setopt($ch, CURLOPT_URL, $urlna);
-						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-					$data = curl_exec($ch);
-
-			preg_match('/<meta property="og:video" content="(.*?)" \/>/', $data, $matches);
+    	$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $urlna);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	$data = curl_exec($ch);
+	preg_match('/<meta property="og:video" content="(.*?)" \/>/', $data, $matches);
 	$smile = $matches[1];
 print_r($smile);
 
