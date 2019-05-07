15c911c2711baec8ddee46db73853319970d6db6
faismuzaki <34159640+faismuzaki@users.noreply.github.com>
2019-05-07 09:15:16 +0700

    Update Getinfouser.php

--- Getinfouser.php
+++ Getinfouser.php
@@ -2,8 +2,8 @@
 /*
     SMILEBOYS (c) 2019
     https://github.com/faismuzaki/Tool-Instagram
-    Made by :3
-*/
+    Made by Faiz Muzaki
+*/	
 	echo "Username Instagram : ";
 	$username = trim(fgets(STDIN));
     	$ch = curl_init();
@@ -17,14 +17,17 @@
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 		$output = curl_exec($ch);
-	preg_match('/<a class="download-btn" href="(.*?)" target="_blank"><i><\/i>Download<\/a>/', $output,$smile);
+		preg_match('/<a class="download-btn" href="(.*?)" target="_blank"><i><\/i>Download<\/a>/', $output,$smile);
+
+	preg_match('/<meta content="(.*?)" name="description" \/>/', $data, $follower);
+	$asw = explode(" ", $follower[1]);
 #Get follower
 			$fix = str_replace(',', '', $asw[0]);
 				$foll = str_replace(",",  " ", $asw[1]);
 #Get Following
 		$following = explode(" ", $asw[2]);
 #Get Total Post
-		$post = $asw[4].$asw[5];
+		$post = $asw[4].' '.$asw[5];
 #Get UserID
 	preg_match('/"id":"(.*?)","/', $data, $userid);
 		$jancok = $userid[1];
@@ -34,6 +37,7 @@
 					'Jumlah Post' => $post,
 					'Foto Profile' => $smile[1]);
 	print_r($result);
+
 
 
 
